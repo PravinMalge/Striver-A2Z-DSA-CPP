d28af37c6e7c44c089a13132d0ca8d3d7b4c8e30
Pravin Malge <pravinmalge123@gmail.com>
2026-03-01 18:44:12 +0530

    Update gitignore and add If-Else code

---
+++ 01_Basics/01>Things_to_Know_in_C++/02_If_Else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    if (n>=18)
+    {
+        cout<<"your young"<<endl;
+    }
+    else{
+        cout<<"your baccha"<<endl;
+    }
+    
+    return 0;
+}
