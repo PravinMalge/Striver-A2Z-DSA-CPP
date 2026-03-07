c3cff98c83a1f6745ae4b9120bdcc5fd447a7413
Pravin Malge <pravinmalge123@gmail.com>
2026-03-07 16:56:28 +0530

    pattern10

---
+++ 01_Basics/02>Logical_Thinking/10_Pattern.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void pattern1(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i>=j)
+            {
+                cout<<"* ";
+            }
+            
+        }
+        cout<<endl;
+    }
+    
+}
+void pattern2(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i<=j)
+            {
+                cout<<"* ";
+            }
+            
+        }
+        cout<<endl;
+    }
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern1(n);
+    pattern2(n);
+    return 0;
+}
