f925fb9ab641575406457633e9e9a3d0a232eaf2
Pravin Malge <pravinmalge123@gmail.com>
2026-03-15 15:13:53 +0530

    pattern12

---
+++ 01_Basics/02>Logical_Thinking/12_Pattern.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n){
+    int space = 2*(n-1);
+    for (int i = 1; i <= n; i++)
+    {
+        // Numbers
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<j;
+        }
+        // Spaces
+        for (int j = 1; j <= space; j++)
+        {
+            cout<<" ";
+        }
+        
+
+        // Numbers
+        for (int j = i; j >= 1; j--)
+        {
+            cout<<j; 
+        }
+        cout<<endl;
+        space=space-2;
+    }
+    
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n);
+    return 0;
+}
